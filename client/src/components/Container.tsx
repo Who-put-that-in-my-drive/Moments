435df929299dc02873a28de5f52df4f9a2ce6cf7
Parth Patel <42159154+patel1643@users.noreply.github.com>
2022-09-29 01:32:58 -0400

    testing chakra ui components

--- client/src/components/Container.tsx
+++ client/src/components/Container.tsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from 'react';
+import { Container, Heading } from '@chakra-ui/react';
+import { Spinner } from '@chakra-ui/react'
 
-
-const Container: React.FC = () => {
-  const [message, setMessage] = useState('Waiting for message from backend...');
+const MainContainer: React.FC = () => {
+  const [message, setMessage] = useState(<Spinner />);
   useEffect(()=>{
     fetch('/test')
     .then(res => res.json())
@@ -10,11 +11,16 @@ const Container: React.FC = () => {
   }, []);
 
   return(
-  <div> 
-    <h1>Hello from Moments team!</h1>
-    <h2>{message}</h2> 
-  </div>)
+    <Container>
+    <div> 
+    <Heading as='h1' size='xl' noOfLines={1}>
+    Hello from Moments team!
+    </Heading>
+      <h2>{message}</h2> 
+    </div>
+    </Container>
+ )
   
 };
 
-export default Container;
+export default MainContainer;
