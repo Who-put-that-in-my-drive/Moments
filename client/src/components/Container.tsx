3debc4cbed0c67c44e8beb603dd545c43b438b60
Michael Pereira <mwpereira07@gmail.com>
2022-09-29 02:38:16 -0400

    Updating test endpoint

--- client/src/components/Container.tsx
+++ client/src/components/Container.tsx
@@ -8,7 +8,7 @@ const MainContainer: React.FC = () => {
   const [message, setMessage] = useState('Connecting to server...');
   const [spinnerDisplay, setSpinnerDisplay] = useState(true)
   useEffect(()=>{
-    fetch('/test')
+    fetch('/api/test')
     .then(res => res.json())
     .then(data => { setMessage(data.data); setSpinnerDisplay(false)})
     .catch(error => console.log(error))
@@ -16,16 +16,16 @@ const MainContainer: React.FC = () => {
 
   return(
     <Container>
-    <div> 
+    <div>
     <Heading as='h1' size='xl' noOfLines={1}>
     Hello from Moments team!
     </Heading>
       <h2>{message}
-      <Spinner size="sm" hidden={!spinnerDisplay} /></h2> 
+      <Spinner size="sm" hidden={!spinnerDisplay} /></h2>
     </div>
     </Container>
  )
-  
+
 };
 
 export default MainContainer;
