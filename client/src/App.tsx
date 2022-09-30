36400a8669880fa4850644a08df7dd22ea67b334
Parth Patel <42159154+patel1643@users.noreply.github.com>
2022-09-30 00:41:10 -0400

    removed apollo gql and added axios

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,23 +1,16 @@
 import React from 'react';
-import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 import './App.css';
 import Container from './components/Container';
 import { ChakraProvider } from '@chakra-ui/react';
-const client = new ApolloClient({
-  cache: new InMemoryCache(),
-  uri: "http://localhost:4000/graphql"
-});
 
 
 function App() {
   return (
-    <ApolloProvider client={client}>
       <ChakraProvider>
         <div className="App">
           <Container />
         </div>
       </ChakraProvider>
-    </ApolloProvider>
 
   );
 }
