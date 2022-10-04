dd73f73f23a555b1b467f5acf2c8c8d2187a6d9b
Parth Patel <42159154+patel1643@users.noreply.github.com>
2022-10-04 03:14:22 -0400

    added routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,22 @@
 import './App.css';
-import { BrowserRouter as Router } from 'react-router-dom';
-import Container from './components/Container';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { ChakraProvider } from '@chakra-ui/react';
+
+// import Container from './components/Container';
+import Login from './pages/Login';
+import Register from './pages/Register';
 
 
 function App() {
     return (
         <ChakraProvider>
             <Router>
+                <Routes>
+                    <Route path='/login' element={<Login />} />
+                    <Route path='/register' element={<Register />} />
+                </Routes>
                 <div className="App">
-                    <Container />
+                    {/* <Container /> */}
                 </div>
             </Router>
         </ChakraProvider>
