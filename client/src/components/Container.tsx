37c9572b04a8c592c0b04091338872cfbb599f24
Parth Patel <42159154+patel1643@users.noreply.github.com>
2022-10-04 14:25:18 -0400

    fixed the indentation

--- client/src/components/Container.tsx
+++ client/src/components/Container.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { Container, Heading , Spinner } from '@chakra-ui/react';
+import { Container, Heading, Spinner } from '@chakra-ui/react';
 import axios from 'axios';
 
 const MainContainer: React.FC = () => {
@@ -7,22 +7,22 @@ const MainContainer: React.FC = () => {
     const [message, setMessage] = useState('Connecting to server...');
     const [spinnerDisplay, setSpinnerDisplay] = useState(true);
     const URL = process.env.REACT_APP_DEV_SERVER_URL || '';
-    useEffect(()=>{
+    useEffect(() => {
         axios.get(`${URL}/api/test`)
-            .then(res =>{
+            .then(res => {
                 const message = res.data;
                 console.log(message);
                 setMessage(message.message);
                 setSpinnerDisplay(false);
             })
             .catch(error => console.log(error.message));
-    },[]); 
+    }, []);
 
-    return(
+    return (
         <Container>
             <div>
                 <Heading as='h1' size='xl' noOfLines={1}>
-                  Hello from Moments team!
+                    Hello from Moments team!
                 </Heading >
                 <Heading size='md'>{message}
                     <Spinner size="md" hidden={!spinnerDisplay} /></Heading>
