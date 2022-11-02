6ac34e23242820cc92b89b4df635911d91bd165c
Michael Caira <mbcaira@gmail.com>
2022-11-02 15:38:09 -0400

    #23 initial user profile, wip

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,25 +1,33 @@
 import './App.css';
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import {
+    BrowserRouter as Router,
+    Route,
+    Routes,
+    Navigate,
+} from 'react-router-dom';
 
 // import Container from './components/Container';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import { Button, useColorMode } from '@chakra-ui/react';
-
+import UserProfile from './pages/UserProfile';
 
 function App() {
     const { colorMode, toggleColorMode } = useColorMode();
     return (
         <>
-            <Button position={'absolute'} onClick={toggleColorMode}>Toggle {colorMode} theme</Button>
+            <Button position={'relative'} onClick={toggleColorMode}>
+                Toggle {colorMode} theme
+            </Button>
             <Router>
                 <Routes>
-                    <Route path='/' element={<Navigate replace to='/login' />} />
-                    <Route path='/login' element={<Login />} />
-                    <Route path='/register' element={<Register />} />
+                    <Route path="/" element={<Navigate replace to="/login" />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/register" element={<Register />} />
+                    {/* TODO: Make profile dynamic with UID */}
+                    <Route path="/profile" element={<UserProfile />} />
                 </Routes>
             </Router>
-
         </>
     );
 }
--- client/src/pages/UserProfile.tsx
+++ client/src/pages/UserProfile.tsx
@@ -1,7 +1,30 @@
+import {
+    Container,
+    FormControl,
+    FormLabel,
+    HStack,
+    Input,
+} from '@chakra-ui/react';
 
 const UserProfile = () => {
     return (
-        <div>UserProfile</div>
+        <Container maxW='lg' ml='1.5rem'>
+            <FormControl>
+                <HStack spacing='1.5rem'>
+                    <div><FormLabel>First Name</FormLabel>
+                        <Input type='text' />
+                    </div>
+                    <div>
+                        <FormLabel>Last Name</FormLabel>
+                        <Input type='text' />
+                    </div>
+                </HStack>
+                <FormLabel>Username</FormLabel>
+                <Input type='text' />
+                <FormLabel>Email Address</FormLabel>
+                <Input type='text' />
+            </FormControl>
+        </Container >
     );
 };
 
