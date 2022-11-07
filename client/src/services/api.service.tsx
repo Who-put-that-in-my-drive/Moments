b1d24bf8c1442d4a3974e19a37d6dcc6425fd669
Michael Pereira <mwpereira07@gmail.com>
2022-11-07 00:59:50 -0500

    Fixing up dependencies, Fixing return types

--- client/src/services/api.service.tsx
+++ client/src/services/api.service.tsx
@@ -3,13 +3,13 @@ import { LoginFormValues } from '../pages/Login';
 import { RegisterFormValues } from '../pages/Register';
 import { getServerUrl } from '../utils/WebsiteUtils';
 
-export const login = async (data: LoginFormValues): Promise<void> => {
+export const login = async (data: LoginFormValues): Promise<Response> => {
     const userData = { email: data.email, password: data.password };
     const URL = getServerUrl();
     return await axios.post(URL + '/api/auth/login', userData);
 };
 
-export const registerUser = async (data: RegisterFormValues): Promise<void> => {
+export const registerUser = async (data: RegisterFormValues): Promise<Response> => {
     const userData = { displayName: data.displayName, email: data.email, password: data.password };
     const URL = getServerUrl();
     return await axios.post(URL + '/api/auth/register', userData);
