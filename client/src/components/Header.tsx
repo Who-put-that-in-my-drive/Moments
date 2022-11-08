819cf4ba89f21df3e93165159d005e0c6605df7c
Parth Patel <42159154+patel1643@users.noreply.github.com>
2022-11-07 19:42:20 -0500

    fixed the user dropdown with truncated text and added theme changer to header

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,28 +1,39 @@
-import { ChevronRightIcon } from '@chakra-ui/icons';
-import { Box, Center, Flex, IconButton, Text } from '@chakra-ui/react';
+import { ChevronRightIcon, MoonIcon, SunIcon } from '@chakra-ui/icons';
+import { Box, Flex, IconButton, Text, useColorMode } from '@chakra-ui/react';
 // eslint-disable-next-line
 import { HeaderProps } from '../utils/ComponentPropTypes';
 
 
 
 const Header = ({ showSidebarButton = true, onShowSidebar }: HeaderProps) => {
+    const { colorMode, toggleColorMode } = useColorMode();
     return (
         <>
             {showSidebarButton && (
-                <Flex bg="gray.200" color="white" justifyContent="center" p={4} shadow='md' zIndex={'10000'}>
-                    <Box flex="1">
+                <Flex color='white' direction={'row'} justifyContent='space-between' p={4} shadow='md' zIndex={'10000'}>
+                    <Box>
                         <IconButton
                             aria-label='button'
-                            colorScheme="blackAlpha"
-                            icon={<ChevronRightIcon h={8} w={8} />}
+                            colorScheme={'gray'}
+                            icon={<ChevronRightIcon color={colorMode === 'light' ? 'gray' : 'white'} h={8} w={8} />}
                             onClick={onShowSidebar}
-                            variant="outline"
+                            variant='outline'
                         />
                     </Box>
-                    <Center flex="1" h="40px">
-                        <Text color={'gray.900'} fontSize="xl">Moments</Text>
-                    </Center>
-                    <Box flex="1" />
+                    <Box paddingTop='.5rem'>
+                        <Text color={colorMode === 'light' ? 'black' : 'white'} fontSize='xl'>Moments</Text>
+                    </Box>
+
+                    <Box>
+                        <IconButton
+                            aria-label='Theme changer'
+                            color={colorMode === 'light' ? 'gray' : 'white'}
+                            colorScheme={'gray'}
+                            icon={colorMode === 'light' ? <MoonIcon /> : <SunIcon />}
+                            onClick={toggleColorMode}
+                        />
+                    </Box>
+
                 </Flex>
             )}
         </>
