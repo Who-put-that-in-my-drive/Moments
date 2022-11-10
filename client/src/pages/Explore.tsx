9938da0d8874954830075516b38f386964d2fdfc
Michael Caira <mbcaira@gmail.com>
2022-11-09 21:42:07 -0500

    test photo card component for folders

--- client/src/pages/Explore.tsx
+++ client/src/pages/Explore.tsx
@@ -1,17 +1,33 @@
-import { SearchIcon, SettingsIcon } from '@chakra-ui/icons';
-import { Flex, Box, Heading, Tab, TabList, Tabs, InputGroup, InputLeftElement, Input, IconButton, Spacer } from '@chakra-ui/react';
+import {SearchIcon, SettingsIcon} from '@chakra-ui/icons';
+import {
+    Box,
+    Flex,
+    Heading,
+    IconButton,
+    Input,
+    InputGroup,
+    InputLeftElement,
+    SimpleGrid,
+    Spacer,
+    Tab,
+    TabList,
+    Tabs
+} from '@chakra-ui/react';
+import {PhotoCard} from '../components/PhotoCard';
 
 export const Explore = () => {
     return (
-        <Flex align={['center', 'center', 'normal', 'normal']} direction='column' p={['1rem', '2rem', '4rem', '4rem']} width='100%'>
+        <Flex align={['center', 'center', 'normal', 'normal']} direction='column' p={['1rem', '2rem', '4rem', '4rem']}
+            width='100%'>
             <Flex align={['center', '', '', '']} marginRight={'4rem'} zIndex={1000}>
                 <Box w='100%'>
-                    <Heading as='h2' noOfLines={2} paddingBottom={['1rem', '2rem', '3rem']} size={['xl', 'xl', '2xl', '3xl']} textAlign={['center', 'center', 'left', 'left']}>
+                    <Heading as='h2' noOfLines={2} paddingBottom={['1rem', '2rem', '3rem']}
+                        size={['xl', 'xl', '2xl', '3xl']} textAlign={['center', 'center', 'left', 'left']}>
                         Explore
                     </Heading>
                     <Flex display='none'>
                         <Box>
-                            <Tabs paddingBottom={['1rem', '', '', '3rem']} >
+                            <Tabs paddingBottom={['1rem', '', '', '3rem']}>
                                 <TabList>
                                     <Tab>All</Tab>
                                     <Tab>Documents</Tab>
@@ -19,23 +35,42 @@ export const Explore = () => {
                                 </TabList>
                             </Tabs>
                         </Box>
-                        <Spacer />
+                        <Spacer/>
                         <Box>
                             <Flex>
                                 <InputGroup maxWidth='16rem'>
-                                    <InputLeftElement pointerEvents='none'><SearchIcon /></InputLeftElement>
-                                    <Input placeholder='Search' type='text' />
+                                    <InputLeftElement pointerEvents='none'><SearchIcon/></InputLeftElement>
+                                    <Input placeholder='Search' type='text'/>
                                 </InputGroup>
-                                <IconButton aria-label='Settings' icon={<SettingsIcon />} ml='0.4rem' />
+                                <IconButton aria-label='Settings' icon={<SettingsIcon/>} ml='0.4rem'/>
                             </Flex>
                         </Box>
                     </Flex>
                 </Box>
             </Flex>
-            <Box w='100%'>
-                <Heading as='h3' noOfLines={2} paddingBottom={['1rem', '2rem', '3rem']} size={['md', 'md', 'xl', '2xl']} textAlign={['center', 'center', 'left', 'left']}>
-                    People
+            <Box scrollBehavior='auto' w='100%'>
+                <Heading as='h3' noOfLines={2} paddingBottom={['1rem', '2rem', '3rem']} size={['md', 'md', 'xl', '2xl']}
+                    textAlign={['center', 'center', 'left', 'left']}>
+                    Collections
                 </Heading>
+                <SimpleGrid marginTop={'1rem'} minChildWidth='15rem'
+                    overflowY='scroll' spacing='2rem'>
+                    <PhotoCard date='' format=''
+                        imageURL='https://cdn.mos.cms.futurecdn.net/wtqqnkYDYi2ifsWZVW2MT4-1200-80.jpg'
+                        name='test' size='10'/>
+                    <PhotoCard date='' format=''
+                        imageURL='https://cdn.mos.cms.futurecdn.net/wtqqnkYDYi2ifsWZVW2MT4-1200-80.jpg'
+                        name='test' size='10'/>
+                    <PhotoCard date='' format=''
+                        imageURL='https://cdn.mos.cms.futurecdn.net/wtqqnkYDYi2ifsWZVW2MT4-1200-80.jpg'
+                        name='test' size='10'/>
+                    <PhotoCard date='' format=''
+                        imageURL='https://cdn.mos.cms.futurecdn.net/wtqqnkYDYi2ifsWZVW2MT4-1200-80.jpg'
+                        name='test' size='10'/>
+                    <PhotoCard date='' format=''
+                        imageURL='https://cdn.mos.cms.futurecdn.net/wtqqnkYDYi2ifsWZVW2MT4-1200-80.jpg'
+                        name='test' size='10'/>
+                </SimpleGrid>
             </Box>
         </Flex>
     );
