33350fcfea494b1becb2bdcf142352162f749616
Parth Patel <42159154+patel1643@users.noreply.github.com>
2022-11-14 19:18:07 -0500

    linter updated these files

--- server/__tests__/health.test.ts
+++ server/__tests__/health.test.ts
@@ -1,10 +1,10 @@
-import app from "../src/index";
-import request from "supertest";
+import app from '../src/index';
+import request from 'supertest';
 
-describe("/health - Simple Health API endpoint", () => {
-    it("/ GET", async () => {
-        const result = await request(app).get("/");
-        expect(result.body.msg).toEqual("Server Online!");
+describe('/health - Simple Health API endpoint', () => {
+    it('/ GET', async () => {
+        const result = await request(app).get('/');
+        expect(result.body.msg).toEqual('Server Online!');
         expect(result.statusCode).toEqual(200);
     });
 });
--- server/jest.config.ts
+++ server/jest.config.ts
@@ -1,28 +1,28 @@
 export default {
     clearMocks: true,
     collectCoverage: true,
-    coverageDirectory: "coverage",
+    coverageDirectory: 'coverage',
     maxWorkers: 1,
     moduleFileExtensions: [
-      "js",
-      "mjs",
-      "cjs",
-      "jsx",
-      "ts",
-      "tsx",
-      "json",
-      "node"
+        'js',
+        'mjs',
+        'cjs',
+        'jsx',
+        'ts',
+        'tsx',
+        'json',
+        'node'
     ],
     testMatch: [
-      "**/__tests__/**/*.[jt]s?(x)",
+        '**/__tests__/**/*.[jt]s?(x)',
     ],
     testTimeout: 30000,
     testPathIgnorePatterns: [
-      "\\\\node_modules\\\\",
-        "utils.ts"
+        '\\\\node_modules\\\\',
+        'utils.ts'
     ],
     transform: {
-        "^.+\\.(ts|tsx)$": ["ts-jest", {"tsConfigFile": "tsconfig.json"}]
+        '^.+\\.(ts|tsx)$': ['ts-jest', {'tsConfigFile': 'tsconfig.json'}]
     },
     preset: 'ts-jest',
     testEnvironment: 'node',
