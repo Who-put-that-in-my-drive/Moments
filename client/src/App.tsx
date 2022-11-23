d5edce71add48019fddc533d88a0a2fb5cc79ff2
jamestang12 <homing.tang@ryerson.ca>
2022-11-22 20:39:06 -0500

    Don't know what to put here

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,6 @@
 import { Button, useColorMode } from '@chakra-ui/react';
-import { BrowserRouter as Router, Navigate, Route, Routes } from 'react-router-dom';
+// import { BrowserRouter as Router, Navigate, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import Landing from './pages/Landing';
 import Dashboard from './pages/Dashboard';
@@ -9,20 +10,20 @@ import { Profile } from './pages/Profile';
 import Register from './pages/Register';
 // import Post from './pages/Post';
 import Uploads from './pages/Uploads';
-import useStore from './store/store';
+// import useStore from './store/store';
 
 
 function App() {
     const { colorMode, toggleColorMode } = useColorMode();
-    const store = useStore();
-    const loggedIn: boolean = store.loggedIn;
+    // const store = useStore();
+    // const loggedIn: boolean = store.loggedIn;
     return (
         <>
             <Button display={'none'} onClick={toggleColorMode} position={'absolute'}>Toggle {colorMode} theme</Button>
             <Router>
                 <Routes>
                     <Route element={<Landing />} path='/' ></Route>
-                    <Route element={<Navigate replace to={loggedIn ? '/dashboard/home' : '/login'} />} path='*' />
+                    {/* <Route element={<Navigate replace to={loggedIn ? '/dashboard/home' : '/login'} />} path='*' /> */}
                     <Route element={<Login />} path='login' />
                     <Route element={<Register />} path='register' />
                     <Route element={<Dashboard />} path='dashboard' >
--- client/src/pages/Explore.tsx
+++ client/src/pages/Explore.tsx
@@ -13,41 +13,162 @@ import {
     TabList,
     Tabs,
     Text,
+    Button
 } from '@chakra-ui/react';
 import { Collection } from '../components/Collection';
+import { AiOutlineRollback } from 'react-icons/ai';
 
 // eslint-disable-next-line no-unused-vars
 import { UserStore } from '../interfaces/UserStore';
 import useStore from '../store/store';
 import { useEffect, useState } from 'react';
-
+import { PhotoCard } from '../components/PhotoCard';
+import { Image } from '../interfaces/Image';
 type TagStructure = {
     count: number,
-    imageUrls: string[],
+    imageData: Image[],
     name: string
+}
+
+type ImageDto = {
+    id: string
+    title: string,
+    format: string,
+    size: string,
+    caption: string,
+    tags: string[],
+    categories: string[],
+    owner: string,
+    location: string,
+    lastModifiedDateTime: number,
+    uploadedDateTime: number,
 }
 
 export const Explore = () => {
     const store: UserStore = useStore();
     const user = store.user;
-    const images = user.images;
-    const [tags, setTags] = useState<TagStructure[]>([]);
+    const images1 = user.images;
+    console.log(images1);
 
+    const dummyImage1 : Image = {
+        caption: 'testing123',
+        categories: ['Yes', 'Yes2'],
+        format: 'png',
+        id: '1',
+        lastModifiedDateTime: 123,
+        location: 'https://images.pexels.com/photos/1323550/pexels-photo-1323550.jpeg?cs=srgb&dl=pexels-simon-berger-1323550.jpg&fm=jpg',
+        owner: 'Parth',
+        size: '12.2 MB',
+        tags: ['Ocean'],
+        title: 'title1',
+        uploadedDateTime:321
+    };
+    const dummyImage2 : Image = {
+        caption: 'testing123',
+        categories: ['Yes', 'Yes2'],
+        format: 'png',
+        id: '1',
+        lastModifiedDateTime: 123,
+        location: 'https://images.pexels.com/photos/1323550/pexels-photo-1323550.jpeg?cs=srgb&dl=pexels-simon-berger-1323550.jpg&fm=jpg',
+        owner: 'Parth',
+        size: '12.2 MB',
+        tags: ['Ocean'],
+        title: 'title1',
+        uploadedDateTime:321
+    };
+    const dummyImage3 : Image = {
+        caption: 'testing123',
+        categories: ['Yes', 'Yes2'],
+        format: 'png',
+        id: '1',
+        lastModifiedDateTime: 123,
+        location: 'https://images.pexels.com/photos/1323550/pexels-photo-1323550.jpeg?cs=srgb&dl=pexels-simon-berger-1323550.jpg&fm=jpg',
+        owner: 'Parth',
+        size: '12.2 MB',
+        tags: ['Ocean'],
+        title: 'title1',
+        uploadedDateTime:321
+    };
+    const dummyImage4 : Image = {
+        caption: 'testing123',
+        categories: ['Yes', 'Yes2'],
+        format: 'png',
+        id: '1',
+        lastModifiedDateTime: 123,
+        location: 'https://images.pexels.com/photos/1323550/pexels-photo-1323550.jpeg?cs=srgb&dl=pexels-simon-berger-1323550.jpg&fm=jpg',
+        owner: 'Parth',
+        size: '12.2 MB',
+        tags: ['Ocean'],
+        title: 'title1',
+        uploadedDateTime:321
+    };
+
+    const dummyImage5 : Image = {
+        caption: 'testing123',
+        categories: ['Yes', 'Yes2'],
+        format: 'png',
+        id: '1',
+        lastModifiedDateTime: 123,
+        location: 'https://images.pexels.com/photos/1323550/pexels-photo-1323550.jpeg?cs=srgb&dl=pexels-simon-berger-1323550.jpg&fm=jpg',
+        owner: 'Parth',
+        size: '12.2 MB',
+        tags: ['People'],
+        title: 'title1',
+        uploadedDateTime:321
+    };
+
+    const dummyImage6 : Image = {
+        caption: 'testing123',
+        categories: ['Yes', 'Yes2'],
+        format: 'png',
+        id: '1',
+        lastModifiedDateTime: 123,
+        location: 'https://images.pexels.com/photos/1323550/pexels-photo-1323550.jpeg?cs=srgb&dl=pexels-simon-berger-1323550.jpg&fm=jpg',
+        owner: 'Parth',
+        size: '12.2 MB',
+        tags: ['People'],
+        title: 'title1',
+        uploadedDateTime:321
+    };
+
+    const images = [dummyImage1, dummyImage2, dummyImage3, dummyImage4, dummyImage5,dummyImage6];
+    
+    const [tags, setTags] = useState<TagStructure[]>([]);
+    const [displayCollection, setDisplayCollection ] = useState<ImageDto[]>([]);
+    const [showCollection, setShowCollection] = useState(0);
+    const [collectionName, setCollectionName] = useState('');
+
+    const onClickBackToCollection = () => {
+        setShowCollection(0);
+    };
 
     const gatherTags = () => {
-        if (images.length > 0) {
-            let gatheredTags: TagStructure[] = [];
-            images.forEach(image => {
-                if (gatheredTags[image.tags] === undefined) {
-                    gatheredTags[image.tags] = {
-                        count: 1,
-                        imageUrls: [image.url],
-                        name: image.tags
-                    };
-                } else {
-                    gatheredTags[image.tags].count++;
-                    gatheredTags[image.tags].imageUrls.push(image.url);
-                }
+        if (images.length > 0 ) {
+            let gatheredTags: TagStructure[] = [];                      
+            images.map(img => {
+                img.tags.map(tag => {                    
+                    let positionIndex = -1;
+                    gatheredTags.forEach((item, index) => {
+                        if(item.name === tag) {
+                            positionIndex = index;
+                            return;
+                        }
+                    });
+                    
+
+                    if(positionIndex === -1) {
+                        gatheredTags.push({
+                            count: 1,
+                            imageData: [img],
+                            name: tag
+                        });
+                    } else {
+                        gatheredTags[positionIndex].count++;
+                        gatheredTags[positionIndex].imageData.push(img);
+                    }
+
+                });
+
             });
             setTags(gatheredTags);
         }
@@ -56,7 +177,7 @@ export const Explore = () => {
     useEffect(() => {
         gatherTags();
     }, []);
-
+    
     return (
         <Flex align={['center', 'center', 'normal', 'normal']} direction='column' p={['1rem', '2rem', '4rem', '4rem']}>
             <Flex align={['center', '', '', '']} marginRight={['', '', '4rem','4rem']} minWidth='100%' zIndex={1000}>
@@ -86,18 +207,42 @@ export const Explore = () => {
                             </Flex>
                         </Box>
                     </Flex>
+                    
                     <Heading as='h2' fontWeight='semibold' marginTop='1rem' noOfLines={2} paddingBottom={['0.5rem', '1rem', '2rem']} size={['md', 'md', 'xl', '2xl']}
                         textAlign={['center', 'center', 'left', 'left']}>
-                        Collections
+                        Collections {showCollection === 0? null: <> : {collectionName} </>}
                     </Heading>
                 </Box>
             </Flex>
-            <SimpleGrid maxH={['65vh', '69vh', '70vh', '75vh']}  minChildWidth='15rem' spacing='2rem'>
+            {showCollection === 0 ? <SimpleGrid maxH={['65vh', '69vh', '70vh', '75vh']}  minChildWidth='15rem' spacing='2rem'>
                 {tags.length > 0 ?
-                    tags.map((tag, index) => <Collection collectionName={tag.name} images={tag.imageUrls} key={index} />)
+                    tags.map((tag, index) => <Collection collectionName={tag.name} count={tag.count} displayCollection={displayCollection} images={tag.imageData} key={index} setCollectionName={setCollectionName} setDisplayCollection={setDisplayCollection} setShowCollection={setShowCollection} thumbnail={tag.imageData[0].location} />)
                     : <Text> No collections created. Add tags to uploaded images to get started.</Text>
                 }
-            </SimpleGrid>
+            </SimpleGrid> : 
+            
+                <Flex align={['center', 'normal', 'normal', 'normal']} direction='column' maxH={'100vh'} p={['1rem', '2rem', '4rem', '3rem']} width='100%'>
+            
+                    <SimpleGrid marginTop={'1rem'} maxH={['67vh', '66vh', '77vh', '77vh']} minChildWidth={['13rem', '13rem', '14rem', '15rem']} overflowY='scroll' spacing='2rem'>
+                
+                        {displayCollection.map(image => {
+                            return (
+                                <PhotoCard
+                                    date={'Nov 9, 2022'}
+                                    format={image.format}
+                                    imageURL={image.location}
+                                    key={image.id}
+                                    name={image.title}
+                                    size={image.size} />);
+                        })}
+
+                    </SimpleGrid>
+            
+            
+                    <Button height='3.5rem' leftIcon={<AiOutlineRollback />} maxW='16rem' minW='15rem' onClick={onClickBackToCollection} style={{marginTop: '50px'}}>Back To Folder</Button>
+
+                </Flex>}
+            
         </Flex>
     );
 };
